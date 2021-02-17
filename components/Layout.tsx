39c4826c6c569ffa9fd022b61fb0a3d7b3f8cf4a
Ros McMahon <rosmcmahon@gmail.com>
2021-02-17 18:46:42 +0100

    barebones GA working

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -7,6 +7,17 @@ type Props = {
   title?: string
 }
 
+import GA4React from "ga-4-react";
+if(typeof window !== 'undefined'){ //process.env.NODE_ENV === 'production'
+  const ga4react = new GA4React('G-L18Q7RWWKN')
+  ga4react.initialize().then(ga4 => {
+    ga4.pageview('path')
+    ga4.gtag('event','pageview', 'path') //or custom gtag event
+  }, err => {
+    console.log(err)
+  })
+}
+
 const Layout = ({ children, title = 'This is the default title' }: Props) => (
   <div>
     <Head>
